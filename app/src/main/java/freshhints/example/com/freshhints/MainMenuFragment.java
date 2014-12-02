cb17df6f450c861952b58919c96354a3ac1bcb8b
acdevine89 <annie.devine@detroitlabs.com>
2014-12-02 13:48:17 -0500

    Adding onCreateView to MainMenuFragment that inflates fragment_main_menu

--- app/src/main/java/freshhints/example/com/freshhints/MainMenuFragment.java
+++ app/src/main/java/freshhints/example/com/freshhints/MainMenuFragment.java
@@ -1,7 +1,11 @@
 package freshhints.example.com.freshhints;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by anniedevine on 12/2/14.
@@ -16,4 +20,10 @@ public class MainMenuFragment extends Fragment {
         mFood = new Food();
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_menu, container, false);
+        return v;
+    }
+    
 }
