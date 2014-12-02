fd5f14fa1bd431d1278223e9c498ee6dc6248acc
acdevine89 <annie.devine@detroitlabs.com>
2014-12-02 13:05:43 -0500

    Creating Food model with getters and setters for name, number of days left, and freshness tips

---
+++ app/src/main/java/freshhints/example/com/freshhints/Food.java
@@ -0,0 +1,35 @@
+package freshhints.example.com.freshhints;
+
+/**
+ * Created by anniedevine on 12/2/14.
+ */
+public class Food {
+
+    private String name;
+    private int daysLeft;
+    private String tips;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDaysLeft() {
+        return daysLeft;
+    }
+
+    public void setDaysLeft(int daysLeft) {
+        this.daysLeft = daysLeft;
+    }
+
+    public String getTips() {
+        return tips;
+    }
+
+    public void setTips(String tips) {
+        this.tips = tips;
+    }
+}
--- app/src/main/java/freshhints/example/com/freshhints/MainActivity.java
+++ app/src/main/java/freshhints/example/com/freshhints/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_main_menu, container, false);
             return rootView;
         }
     }
