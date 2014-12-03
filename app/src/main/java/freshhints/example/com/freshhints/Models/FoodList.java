7dfcd66e44d87042fce89fbfe6c26e7ccb50ae0c
acdevine89 <annie.devine@detroitlabs.com>
2014-12-03 14:33:32 -0500

    Adding food objects to FoodList array list

--- app/src/main/java/freshhints/example/com/freshhints/Models/FoodList.java
+++ app/src/main/java/freshhints/example/com/freshhints/Models/FoodList.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
  */
 public class FoodList extends ArrayList {
 
-    Food[] FoodList = new ArrayList<Food>;
-
     Food banana = new Food("banana", 5, "keep bananas fresh");
     Food peach = new Food("peach", 6, "keep peaches fresh");
     Food blackberries = new Food("blackberries", 3, "keep blackberries fresh");
@@ -16,4 +14,7 @@ public class FoodList extends ArrayList {
     Food kiwi = new Food("kiwi", 7, "keep kiwi fresh");
     Food lemon = new Food("lemon", 9, "keep lemon fresh");
 
+    public void addFoodToArrayList() {
+        Food[] foodList = {banana, peach, blackberries, apple, kiwi, lemon};
+    }
 }
