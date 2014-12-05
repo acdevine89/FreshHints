10bfbff80b992d3420f7a3253bd7977dd1a714ce
acdevine89 <annie.devine@detroitlabs.com>
2014-12-04 21:09:56 -0500

    Putting list view in its own xml file as fragment_view_food_list, and refactoring fragment_view_food xml as fragment_view_food_row

--- app/src/main/java/freshhints/example/com/freshhints/Fragments/ViewFoodFragment.java
+++ app/src/main/java/freshhints/example/com/freshhints/Fragments/ViewFoodFragment.java
@@ -2,11 +2,9 @@ package freshhints.example.com.freshhints.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import freshhints.example.com.freshhints.R;
 
@@ -22,7 +20,7 @@ public class ViewFoodFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_view_food, container, false);
+        View v = inflater.inflate(R.layout.fragment_view_food_row, container, false);
         return v;
     }
 
