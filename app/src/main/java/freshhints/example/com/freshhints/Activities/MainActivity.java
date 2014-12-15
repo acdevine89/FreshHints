8eb676393c48c873a622614b6da1a54792226e4f
acdevine89 <annie.devine@detroitlabs.com>
2014-12-15 14:58:44 -0500

    Small changes to main activity

--- app/src/main/java/freshhints/example/com/freshhints/Activities/MainActivity.java
+++ app/src/main/java/freshhints/example/com/freshhints/Activities/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends FragmentActivity implements FragmentController
 //        }
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
