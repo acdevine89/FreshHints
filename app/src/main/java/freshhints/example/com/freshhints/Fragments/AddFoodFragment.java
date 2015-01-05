0e0616550a5965bdfba2f0d080f010967e8b858b
acdevine89 <annie.devine@detroitlabs.com>
2015-01-05 10:28:35 -0500

    Adding ReminderProvider class with constant COLUMN_ROWID

--- app/src/main/java/freshhints/example/com/freshhints/Fragments/AddFoodFragment.java
+++ app/src/main/java/freshhints/example/com/freshhints/Fragments/AddFoodFragment.java
@@ -1,6 +1,7 @@
 package freshhints.example.com.freshhints.fragments;
 
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
