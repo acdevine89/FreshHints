bf04f6e7ca24b280b5dd6294e730701f404d0b54
acdevine89 <annie.devine@detroitlabs.com>
2015-01-05 14:49:55 -0500

    Adding constants to FoodProvider for database version number, database name and table name

--- app/src/main/java/freshhints/example/com/freshhints/Models/FoodProvider.java
+++ app/src/main/java/freshhints/example/com/freshhints/Models/FoodProvider.java
@@ -1,10 +1,47 @@
 package freshhints.example.com.freshhints.models;
 
 import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 
 /**
  * Created by anniedevine on 1/5/15.
  */
 public class FoodProvider extends ContentProvider {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "food_data";
+    private static final String DATABASE_TABLE = "food_table";
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
 }
