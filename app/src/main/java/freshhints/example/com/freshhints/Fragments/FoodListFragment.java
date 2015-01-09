2bed05acd3e1fbb5c5e1945f9b1eb7a8db8983c2
acdevine89 <annie.devine@detroitlabs.com>
2015-01-09 01:13:05 -0500

    Adding potential methods for onListItemClick and onContextItemSelected that will allow user to long press a food in the list in order to delete it

--- app/src/main/java/freshhints/example/com/freshhints/Fragments/FoodListFragment.java
+++ app/src/main/java/freshhints/example/com/freshhints/Fragments/FoodListFragment.java
@@ -1,9 +1,16 @@
 package freshhints.example.com.freshhints.fragments;
 
 import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v7.internal.widget.AdapterViewCompat;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 import freshhints.example.com.freshhints.R;
@@ -31,6 +38,37 @@ public class FoodListFragment extends ListFragment implements LoaderManager.Load
                 null, foodInfoToBeDisplayed, fieldsToBindFoodInfo, 0);
         setListAdapter(mAdapter);
         getLoaderManager().initLoader(0, null, this);
-
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setEmptyText(getResources().getString(R.string.no_food_yet));
+        registerForContextMenu(getListView());
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        // This will place into the intent the ID of the task to be edited. The class that is
+        // needed will inspect the intent and if it finds the ID, will attempt to allow the user
+        // to edit the task
+        //startActivity(new Intent(getActivity(), need a class here));
+        //.putExtra(FoodProvider.COLUMN_ROWID, id));
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case R.id.menu_delete:
+//                AdapterViewCompat.AdapterContextMenuInfo info =
+//                        (AdapterViewCompat.AdapterContextMenuInfo) item.getMenuInfo();
+//                getActivity().getContentResolver().delete(
+//                        ContentUris.withAppendedId(FoodProvider.CONTENT_URI, info.id), null, null);
+//                return true;
+//        }
+//        return super.onContextItemSelected(item);
+    }
+
 }
